f96ff2c53362a97c71f4b6a1a8b6df7598b6ce0a
MaishaTahsin <rubaitreshad@gmail.com>
2025-06-17 17:24:12 +0600

    implement google sign-in

--- src/_firebase_auth_steps.js
+++ src/_firebase_auth_steps.js
@@ -2,4 +2,7 @@
  * 1. create a project in firebase console
  * 2 in the proj : install firebase
  * 3. bring firebase config into your project (from site)
+ * 4. by using useAuth, create the auth and export it
+ * 5. log in page: create a provider GoogleAuthProvider
+ * 6. use sign in with popup and pass auth and provider
  */
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../../firebase/firebase.init";
 
 const Login = () => {
+  const provider = new GoogleAuthProvider();
+
   const handleGoogleSignIn = () => {
-    console.log("google sign in clicked");
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
+
   return (
     <div>
       <h2>Please Login</h2>
